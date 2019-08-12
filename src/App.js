892d96d8928dc6ef18bff7f127a04494d7177017
Syava7 <syava77787@gmail.com>
2019-08-12 14:31:15 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Car from './Car/Car.js' 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  state = {
+    cars: [
+      {name: 'Audi', year: 2020},
+      {name: 'Mazda', year: 2018},
+      {name: 'Ford', year: 2019}
+    ],
+    pageTitle: 'React components'
+  }
+
+  onChangeTitle = () => {
+    console.log('click')
+  }
+
+  render() {
+
+    const cars = this.state.cars
+
+    return (
+      <div className="App">
+        <h1>{this.state.pageTitle}</h1>
+
+        <button onClick={this.onChangeTitle}>Click me</button>
+
+        <Car name={cars[0].name} year={cars[0].year} />
+        <Car name={cars[1].name} year={cars[1].year} />
+        <Car name={cars[2].name} year={cars[2].year} />
+      </div>
+    );
+  }
 }
 
 export default App;

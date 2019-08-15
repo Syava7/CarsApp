851cd38a6d907cfd198138f32fd4d4bc6c12941a
Syava7 <syava77787@gmail.com>
2019-08-15 15:52:17 +0300

    changes

---
+++ src/Counter/Counter.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Aux from '../hoc/Auxo'
+
+export default class Counter extends Component {
+
+  state = {
+    counter: 0
+  }
+
+  addCounter = () => {
+    // this.setState({
+    //   counter: this.state.counter + 1
+    // })
+
+    this.setState((prevState) => {
+      return {
+        counter: prevState.counter + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Aux>
+        <h2>Counter {this.state.counter}</h2>
+        <button 
+          onClick={this.addCounter}>
+          +
+        </button>
+        <button 
+          onClick={() => this.setState({counter: this.state.counter - 1})}>
+          -
+        </button>
+      </Aux>
+    )
+  }
+}
---
+++ src/hoc/Auxo.js
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+const Auxo = (props) => {
+  return props.children
+}
+
+export default Auxo
